320913fb4653c4008b243e2e548250e5dcd29ae1
chakrosch <georg.eickelpasch@gmx.de>
2019-04-29 22:05:49 +0900

    Implemented working JSON Post in background service

--- GroupPowerSave/app/src/main/java/com/example/grouppowersave/GPSService.java
+++ GroupPowerSave/app/src/main/java/com/example/grouppowersave/GPSService.java
@@ -2,19 +2,13 @@ package com.example.grouppowersave;
 
 
 import android.app.IntentService;
-import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
 import android.util.Log;
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import org.json.JSONObject;
 
@@ -25,11 +19,11 @@ public class GPSService extends IntentService {
         super("GPSService");
     }
 
-    public URL url;
+    public static URL url;
 
     {
         try {
-            url = new URL("http://jsonplaceholder.typicode.com/");                      //enter URl here
+            url = new URL("https://webhook.site/90286341-1ec0-4499-990b-1cb2e7dcaa7e");                      //enter URl here
         } catch (MalformedURLException e) {
             Log.d("Service","URL failed");
         }
@@ -42,7 +36,7 @@ public class GPSService extends IntentService {
         connectToServer();
 
     }
-public void connectToServer(){
+public static void connectToServer(){
         final JSONObject jsonO = new JSONObject();
         final String jsonS = jsonO.toString();
 
@@ -50,26 +44,13 @@ public void connectToServer(){
 
             @Override
             protected String doInBackground(Void... voids) {
-                DataOutputStream wr = null;
                 try {
-                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
-                    con.setRequestMethod("POST");
-
-                        wr = new DataOutputStream(con.getOutputStream());
-                        wr.writeBytes("data");
-                        wr.flush();
-                        wr.close();
-                    } catch(IOException exception) {
-                      Log.d("service","send json failed"); // more accurate error handling implementable if required
-                    }finally{
-                    try {
-                        wr.close();
-                    } catch (IOException e) {
-                        Log.d("service","closing dataoutputstream failed");
-                    }
+                    HttpClientExample hce = new HttpClientExample();
+                    String body = hce.post("https://webhook.site/90286341-1ec0-4499-990b-1cb2e7dcaa7e", "11data=test data");
+                    System.out.println(body);
+                } catch(IOException ioe) {
+                    ioe.printStackTrace();
                 }
-
-
                 return null;
             }
         }.execute();
--- GroupPowerSave/app/src/main/java/com/example/grouppowersave/MainActivity.java
+++ GroupPowerSave/app/src/main/java/com/example/grouppowersave/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.grouppowersave;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
 
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    public static URL url;
+
+    {
+        try {
+            url = new URL("https://webhook.site/90286341-1ec0-4499-990b-1cb2e7dcaa7e");                      //enter URl here
+        } catch (MalformedURLException e) {
+            Log.d("Service","URL failed");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startService(new Intent(this, GPSService.class));
     }
-
-
 }
