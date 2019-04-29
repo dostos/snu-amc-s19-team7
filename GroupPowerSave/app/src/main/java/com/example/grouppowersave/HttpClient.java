85968c1bfbda0e97b8f7198b76780f6be177c715
chakrosch <georg.eickelpasch@gmx.de>
2019-04-29 22:21:29 +0900

    Fixed merch conflict

--- GroupPowerSave/app/src/main/java/com/example/grouppowersave/HttpClient.java
+++ GroupPowerSave/app/src/main/java/com/example/grouppowersave/HttpClient.java
@@ -8,9 +8,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-//import javax.net.ssl.HttpsURLConnection;
 
-public class HttpClientExample {
+public class HttpClient {
 
 
 
