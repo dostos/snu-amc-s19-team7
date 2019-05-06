c55e7cbc5d94222e403d130cf6aff73de12f1d54
chakrosch <georg.eickelpasch@gmx.de>
2019-05-06 19:19:55 +0900

    Added various communication methods -provide location -receive location -update role

--- GroupPowerSave/app/src/main/java/com/example/grouppowersave/GPSService.java
+++ GroupPowerSave/app/src/main/java/com/example/grouppowersave/GPSService.java
@@ -14,6 +14,7 @@ import android.location.LocationProvider;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.util.Log;
 
@@ -32,14 +33,21 @@ public class GPSService extends IntentService implements SensorEventListener {
     private SensorManager mSensorManager;
     LocationManager mLocationManager;
     Context mContext;
-
+    static HttpClient client;
+    Handler handler;
+    Runner runner;
+    static Location currentLocation;
+    static String uniqueUserID = "IDnotSet";
+    static int groupStatus = 0;
     LocationListener locationListenerGPS = new LocationListener() {
         @Override
         public void onLocationChanged(android.location.Location location) {
+            currentLocation = location;
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
             String msg = "New Latitude: " + latitude + "New Longitude: " + longitude;
             Log.e("location", msg);
+            Log.e("locationString", currentLocation.toString());
 
         }
 
@@ -59,7 +67,9 @@ public class GPSService extends IntentService implements SensorEventListener {
     @Override
     protected void onHandleIntent(Intent workIntent) {
         //execute code here, information can be passed to this methode via the intent, but we won't use it most likely
+        //INITIALISATION
         Log.e("GPSService", "started");
+        client = new HttpClient();
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         //have to change position of registerListener when we want to use accelerometer.
         mSensorManager.registerListener(this,
@@ -75,14 +85,23 @@ public class GPSService extends IntentService implements SensorEventListener {
         } catch (SecurityException e) {
             Log.e("SecurityException", e.toString());
         }
-
+        handler = new Handler();
+        runner = new Runner();
+        handler.post(runner);
         connectToServer();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runner);
+        Log.e("Background Service: ","Terminated");
     }
 
     public static void connectToServer() {
         final JSONObject jsonO = new JSONObject();
         try {
-            jsonO.put("ID", "1");
+            jsonO.put("register", "Dieser Wert hat keinen Sinn, aber ich schreib ihn mal auf deutsch.");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -93,9 +112,10 @@ public class GPSService extends IntentService implements SensorEventListener {
             @Override
             protected String doInBackground(Void... voids) {
                 try {
-                    HttpClient client = new HttpClient();
-                    String body = client.post(url, jsonS);
-                    Log.e("Sever Answer:", body);
+
+                    String body = client.register(url, jsonS);
+                    Log.e("UniqueID (registration)", body);
+                    uniqueUserID = body;
                 } catch (IOException ioe) {
                     ioe.printStackTrace();
                 }
@@ -167,5 +187,97 @@ public class GPSService extends IntentService implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+    public static void providePosition() {
+        final JSONObject jsonO = new JSONObject();
+        try {
+            if(currentLocation != null){
+            jsonO.put("user-data", uniqueUserID+currentLocation.toString());}
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final String jsonS = jsonO.toString();
+
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    Log.e("ProvideLocationToServer", jsonS);
+                    client.providePosition(url, jsonS);
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+    public static void receivePosition(){
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    String pos = client.getPosition(url, uniqueUserID);
+                    Log.e("LocationReceived", pos);
+                    currentLocation = stringToLocation(pos);
+
+
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+    public static void receiveGroupStatus(){
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    String ans = client.ping(url, uniqueUserID);
+                    Log.e("GroupStatusReceived", ans);
+                    groupStatus = Integer.parseInt(ans);
+
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    //location.toString fromat looks like gps 75.000000,75.000000 acc=500 et=+1d22h29m13s93ms mock
+    public static Location stringToLocation(String locS){
+        char[] locC = locS.toCharArray();
+        String locSpre = "";
+        for(int i = 1; i< locS.length(); i++){
+            if(!(locC[i] != '0' && locC[i] != '1' && locC[i] != '2' && locC[i] != '3' && locC[i] != '4' && locC[i] != '5' && locC[i] != '6' && locC[i] != '7' &&locC[i] != '8' && locC[i] != '9' && locC[i] != ' ' && locC[i] != ',' && locC[i] != '.')){
+                locSpre = locSpre + locC[i];
+            }
+        }
+        String[] locA = locSpre.split(" ");
+        String[] longlatS = locA[1].split(",");
+        Location loc = new Location(currentLocation);
+        loc.setLatitude(Double.parseDouble(longlatS[0]));
+        loc.setLongitude(Double.parseDouble(longlatS[1]));
+        loc.setAccuracy(Float.parseFloat(locA[2]));
+        //Time has format like +1d22h29m13s93ms - not needed but could be added
+        return loc;
+    }
+    public class Runner implements Runnable {
+        //Should run every 5 seconds
+        @Override
+        public void run() {
+            Log.d("AndroidClarified", "Running");
+            handler.postDelayed(this, 1000 * 5);
+            receiveGroupStatus();
+            if(groupStatus == 0){
+                providePosition();
+            }else {
+                receivePosition();
+            }
+        }
     }
 }
